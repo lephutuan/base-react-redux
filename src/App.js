856345314c65defc7dc6336b4f3e26c8217b0571
lephutuan <lephutuan2810@gmail.com>
2026-03-30 20:56:46 +0700

    State

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 import MyComponent from "./components/MyComponent";
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
-
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>Hello Le Phu Tuan</p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-    <div>
-      Hello word
-      <MyComponent></MyComponent>
-    </div>
-  );
-};
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello word bla bla
+        <MyComponent></MyComponent>
+      </div>
+    );
+  }
+}
+// const App = () => {
+//   const count = useSelector((state) => state.counter.count);
+//   const dispatch = useDispatch();
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello Le Phu Tuan</p>
+//         <div>Count = {count}</div>
+//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//       </header>
+//     </div>
+//   );
+// };
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,17 @@
 import React from "react";
 class MyComponent extends React.Component {
+  //State là một object, nó sẽ lưu trữ tất cả các giá trị mà component cần để render ra giao diện
+  state = {
+    name: "Le Phu Tuan",
+    age: 22,
+    address: "Hue",
+  };
   //JSX
   render() {
     return (
       <div>
-        My first component
-        {Math.random()}
+        My name is {this.state.name}, I am {this.state.age} years old, I am
+        from {this.state.address}
       </div>
     );
   }
