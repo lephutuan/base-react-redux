aaa36f6ee44b9b6573c21446d1f3dd8e77ea1ef0
lephutuan <lephutuan2810@gmail.com>
2026-03-30 21:31:18 +0700

    Form in React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,22 +6,31 @@ class MyComponent extends React.Component {
     age: 22,
     address: "Hue",
   };
-  handleClick = (event) => {
-    console.log("click me");
-    // console.log("My name is", this.state.name);
-    //setState
+  // handleClick = (event) => {
+  //   console.log("click me");
+  //   // console.log("My name is", this.state.name);
+  //   //setState
+  //   this.setState({
+  //     name: "Tuan Le Phu",
+  //     age: Math.floor(Math.random() * 100 + 1),
+  //   });
+  // };
+
+  // handleMouseOver(event) {
+  //   console.log(event.target);
+  //   console.log(event);
+  //   console.log("I am", this.state.age, " years old");
+  // }
+  handleOnChangeInput = (event) => {
     this.setState({
-      name: "Tuan Le Phu",
-      age: Math.floor(Math.random() * 100 + 1),
+      name: event.target.value,
     });
   };
 
-  handleMouseOver(event) {
-    console.log(event.target);
-    console.log(event);
-    console.log("I am", this.state.age, " years old");
-  }
-
+  handleOnSubmitForm = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
   //JSX
   render() {
     return (
@@ -29,15 +38,22 @@ class MyComponent extends React.Component {
         My name is {this.state.name}, I am {this.state.age} years old, I am from{" "}
         {this.state.address}
         <div>
-          <button onClick={this.handleClick}>Click me</button>
+          {/* <button onClick={this.handleClick}>Click me</button> */}
+          <form onSubmit={(event) => this.handleOnSubmitForm(event)}>
+            <input
+              type="text"
+              onChange={(event) => this.handleOnChangeInput(event)}
+            />
+            <button>Submit</button>
+          </form>
           <br></br>
-          <button
+          {/* <button
             onMouseOver={(event) => {
               this.handleMouseOver(event);
             }}
           >
             Hover
-          </button>
+          </button> */}
         </div>
       </div>
     );
