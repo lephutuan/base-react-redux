2b64f82a875dd8fe555983c9e9f65d4707fceb52
lephutuan <lephutuan2810@gmail.com>
2026-03-30 21:49:16 +0700

    Component cho lồng Component con

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,60 +1,12 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 class MyComponent extends React.Component {
   //State là một object, nó sẽ lưu trữ tất cả các giá trị mà component cần để render ra giao diện
-  state = {
-    name: "Le Phu Tuan",
-    age: 22,
-    address: "Hue",
-  };
-  // handleClick = (event) => {
-  //   console.log("click me");
-  //   // console.log("My name is", this.state.name);
-  //   //setState
-  //   this.setState({
-  //     name: "Tuan Le Phu",
-  //     age: Math.floor(Math.random() * 100 + 1),
-  //   });
-  // };
 
-  // handleMouseOver(event) {
-  //   console.log(event.target);
-  //   console.log(event);
-  //   console.log("I am", this.state.age, " years old");
-  // }
-  handleOnChangeInput = (event) => {
-    this.setState({
-      name: event.target.value,
-    });
-  };
-
-  handleOnSubmitForm = (event) => {
-    event.preventDefault();
-    console.log(this.state);
-  };
-  //JSX
   render() {
     return (
       <div>
-        My name is {this.state.name}, I am {this.state.age} years old, I am from{" "}
-        {this.state.address}
-        <div>
-          {/* <button onClick={this.handleClick}>Click me</button> */}
-          <form onSubmit={(event) => this.handleOnSubmitForm(event)}>
-            <input
-              type="text"
-              onChange={(event) => this.handleOnChangeInput(event)}
-            />
-            <button>Submit</button>
-          </form>
-          <br></br>
-          {/* <button
-            onMouseOver={(event) => {
-              this.handleMouseOver(event);
-            }}
-          >
-            Hover
-          </button> */}
-        </div>
+        <UserInfo />
       </div>
     );
   }
