590aff08e7f876910ed3b394f6cc5692c36ee210
lephutuan <lephutuan2810@gmail.com>
2026-03-31 08:47:32 +0700

    Condition output

--- src/components/MyInfo.js
+++ src/components/MyInfo.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { use } from "react";
 
 class MyInfo extends React.Component {
+  state = {
+    isShowHide: true,
+  };
+  handleShowHide = () => {
+    this.setState({
+      isShowHide: !this.state.isShowHide,
+    });
+  };
   render() {
     //Destructuring
     const { listUsers } = this.props;
     return (
       <div>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <div>Name: {user.name},</div>
-              <div>Age: {user.age}</div>
-              <hr></hr>
-            </div>
-          );
-        })}
+        <div>
+          <p
+            onClick={() => {
+              this.handleShowHide();
+            }}
+          >
+            {this.state.isShowHide === true
+              ? "Hide list users"
+              : "Show list users"}
+          </p>
+        </div>
+        {this.state.isShowHide === true && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 20 ? "green" : "red"}>
+                  <div>Name: {user.name},</div>
+                  <div>Age: {user.age}</div>
+                  <hr></hr>
+                </div>
+              );
+            })}
+          </div>
+        )}
       </div>
     );
   }
