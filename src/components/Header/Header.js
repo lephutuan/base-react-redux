a1554be328d99f6fec5556acfa1fa902daf11fb4
lephutuan <lephutuan2810@gmail.com>
2026-04-01 14:21:04 +0700

    Add Home component, update Header for navigation, and style active link color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,9 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">TuanQuiz</Navbar.Brand>
+        <NavLink className="navbar-brand" to="/">
+          TuanQuiz
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -24,9 +26,9 @@ function Header() {
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log in</NavDropdown.Item>
+              <NavDropdown.Item>Log out</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,4 @@
+const Home = (props) => {
+  return <div>This is HomePage</div>;
+};
+export default Home;
