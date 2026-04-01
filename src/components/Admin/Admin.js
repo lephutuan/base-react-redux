722dfffc1eca9081b4bd866aba08b6717b9719f6
lephutuan <lephutuan2810@gmail.com>
2026-04-01 14:02:24 +0700

    Implement routing

---
+++ src/components/Admin/Admin.js
@@ -0,0 +1,4 @@
+const Admin = (props) => {
+  return <div>This is Admin</div>;
+};
+export default Admin;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
@@ -11,9 +12,15 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#users">Users</Nav.Link>
-            <Nav.Link href="#admin">Admin</Nav.Link>
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+            <Link className="nav-link" to="/users">
+              Users
+            </Link>
+            <Link className="nav-link" to="/admins">
+              Admin
+            </Link>
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
